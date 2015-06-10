754feceb314e3d411be25853da9c69fb6d9f1427
mohamed Issaq <minhajraz10@yahoo.com>
2015-06-10 18:15:29 +0100

    create instance variable and create a test for it before hand

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+
 describe DockingStation do
 	it { is_expected.to respond_to :release_bike }
 	
@@ -7,5 +8,18 @@ describe DockingStation do
 		expect(bike).to be_working
 	end
 
-	it { is_expected.to respond_to(:dock).with(1).argument }
+	  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  describe 'release_bike' do 
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+
+      it 'releases working bikes' do
+        subject.dock Bike.new
+        bike = subject.release_bike
+        expect(bike).to be_working
+      end
+    end
+  end
 end
